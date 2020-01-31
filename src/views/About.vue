86da865d0c4befa5864e514eba9eb79c9d06f6b5
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2020-01-31 12:39:55 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,10 +3,11 @@
     <h1>Create XML for Edgeview</h1>
     <h2>For Edgemarc upgrade</h2>
     <br>
-    <textarea v-model="name" placeholder="add multiple lines"></textarea>
+    <span>Edgemarcs List:</span>
+    <p style="white-space: pre-line;">{{ edgemarc }}</p>
+    <br>
+    <textarea v-model="edgemarc" placeholder="add multiple lines"></textarea>
     <textarea v-model="address" placeholder="add multiple lines"></textarea>
     <br>
-    <span>Edgemarcs List:</span>
-    <p style="white-space: pre-line;">{{ name }}</p>
   </div>
 </template>
