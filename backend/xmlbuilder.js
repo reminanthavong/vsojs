dcdee562d7c35aedb65b220efdb2c44087c9aa5d
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2020-01-31 16:03:36 -0500

    Update xmlbuilder.js

--- backend/xmlbuilder.js
+++ backend/xmlbuilder.js
@@ -6,14 +6,14 @@ const generate = async (request, response) => {
  var eIp = request.body.address
   var xml = builder.create('EDGEVIEW')
   .ele('EDGEMARC')
-    .ele('NAME', eName)
-    .ele('ADDRESS', eIp)
+    .ele('NAME', eName).up()
+    .ele('ADDRESS', eIp).up()
   .end({ pretty: true});
  
 console.log(xml);
 //response.send(JSON.stringify(xml));
  response.redirect(
-        "/createxml?success=" + encodeURIComponent(xml)
+        "/createxml" + encodeURIComponent(xml)
       );
 };
 
