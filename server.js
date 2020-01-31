0aca2916dffcc2c66ee675797664d44fe5e54b38
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2020-01-31 15:46:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,5 +23,6 @@ const enableCrossDomain = function(req, res, next) {
 app.use(enableCrossDomain)
 // Fonction Login
 router.post('/makexml', xmlcode.generate)
+app.user(router)
 const port = process.env.PORT || 80;
 app.listen(port);
