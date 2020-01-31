bf33180fa91c191d213dacc16f3e757a860b1748
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2020-01-31 12:32:23 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,11 @@
   <div class="about">
     <h1>Create XML for Edgeview</h1>
     <h2>For Edgemarc upgrade</h2>
+    <br>
+    <textarea v-model="name" placeholder="add multiple lines"></textarea>
+    <textarea v-model="address" placeholder="add multiple lines"></textarea>
+    
+    <span>Edgemarcs List:</span>
+    <p style="white-space: pre-line;">{{ name }} || {{ address }}</p>
   </div>
 </template>
