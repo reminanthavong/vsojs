8bfde590eebbe3e5e1d9960ac70defb98ddbccb2
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2020-02-05 12:09:54 -0500

    Update xmlbuilder.js

--- backend/xmlbuilder.js
+++ backend/xmlbuilder.js
@@ -3,17 +3,17 @@ var builder = require('xmlbuilder');
 const generate = async (request, response) => {
  var eName = request.body.name
  var eIp = request.body.address
-  var xml = builder.create('EDGEVIEW')
-   var lines = eName.split('\n');
+    var lines = eName.split('\n');
    var lines2 = eIp.split('\n');
+  var root = builder.create('EDGEVIEW')
 for(var i = 0;i < lines.length;i++){
    //console.log(lines[i])
-    xml.ele('EDGEMARC')
-    xml.ele('NAME', lines[i]).up()
-    xml.ele('ADDRESS', lines2[i]).up()
+    var item = root.ele('EDGEMARC');
+    item.ele('NAME', lines[i]).up()
+    item.ele('ADDRESS', lines2[i]).up()
     
 }
- xml.end({ pretty: true});
+ var xml = root.end({ pretty: true});
 console.log(xml);
 var result = {};
 result.list = xml;
